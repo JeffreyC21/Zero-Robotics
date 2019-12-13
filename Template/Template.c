ec182ffafc2b337c37667641ff52b29afb5f41b6
JeffreyC21 <58833367+JeffreyC21@users.noreply.github.com>
2019-12-13 12:27:37 +1100

    Add files via upload

---
+++ Template/Template.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(int argc, char *argv[]){
+    
+    printf("Hello World\n");
+    
+    return EXIT_SUCCESS;
+}
+/* To execute code:
+    1. bash
+    2. ctrl + `
+    3. gcc -Wall -Werror -o file filename.c
+    4. ./file
+    5. gcc line compiles
+    6. ./ executes 
+*/
